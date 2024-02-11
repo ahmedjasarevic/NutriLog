b40c5f389f5ef1a1437fcbff29d26b10edd719d4
Ahmed Jasarevic <66922656+ahmedjasarevic@users.noreply.github.com>
2024-02-11 11:29:32 +0100

    Fix bug with login and add links to navbar

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,9 +2,12 @@
 from django.urls import path
 from . import views
 
+app_name= 'accounts'
+
+
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login/', views.login_user, name='login'),
-    path('logout/', views.logout_user, name='logout'),
-    path('profile/', views.profile_view, name='logout'),
+    path('login/', views.login_user, name='login_user'),
+    path('logout/', views.logout_user, name='logout_user'),
+    path('profile/', views.profile_view, name='profile_view'),
 ]
